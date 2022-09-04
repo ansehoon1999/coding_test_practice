f46aeca7a62fee557d0d0b8e0739b1812e2b0354
ansehun <63048392+ansehoon1999@users.noreply.github.com>
2022-09-04 11:53:04 +0900

    Update 1874.py

--- 백준 문제 풀이 모음/class 2/1874.py
+++ 백준 문제 풀이 모음/class 2/1874.py
@@ -1,33 +1,86 @@
 
-class stack() :
-    def __init__(self) :
-        self.stack = []
+# class stack() :
+#     def __init__(self) :
+#         self.stack = []
 
-    def push (self, data) :
-        self.stack.append(data)
+#     def push (self, data) :
+#         self.stack.append(data)
     
-    def pop (self) :
-        self.stack.pop()
+#     def pop(self) :
+#         self.stack.pop()
+    
+# import sys
+# n = int(sys.stdin.readline())
+# l = []
 
+# result1 = []
+# result2 = []
+
+# for i in range(n) : #[4, 3, 6, 8, 7, 5, 2, 1]
+#     l.append(int(sys.stdin.readline()))
+
+#  #[4, 3, 6, 8, 7, 5, 2, 1]
+
+# i = 1
+# while True :
+#     if i == n + 2 :
+#         if len(l) == 0 :
+#             finish = 'ok'
+#         else :
+#             finish = 'no'
+        
+#         break
+
+
+    
+#     if len(result1) == 0 :
+#         result1.append(i)
+#         result2.append('+')
+#         i += 1
+#         continue
+
+#     if l[0] == result1[-1] :
+#         l.pop(0)
+#         result1.pop(-1)
+#         result2.append('-')
+
+#     elif l[0] != result1[-1] :
+#         result1.append(i)
+#         result2.append('+')
+#         i += 1
+
+# if finish == 'ok' :
+#     for i in result2 :
+#         print(i)
+# else :
+#     print('NO')
+    
+
+count = 1
+stack = []
+op = []
+temp = True
 import sys
-num = int(sys.stdin.readline())
-l = []
-stk = stack()
 
-for i in range(num) :
-    l.append(int(sys.stdin.readline()))
+N = int(sys.stdin.readline())
+for i in range(N) :
+    num = int(sys.stdin.readline())
+    while count <= num :
+        stack.append(count)
+        op.append('+')
+        count += 1
+    
+    if stack[-1] == num :
+        stack.pop()
+        op.append('-')
+    
+    else :
+        temp = False
+        break
 
-result = []
-first = l[0]
-#l은 수열
-for i in range(1, first + 1, 1) :
-    stk.push(i)
-    result.append('+')
+if temp :
+    for i in op :
+        print(i)
+else :
+    print('NO')
 
-
-
-l.remove(l[0])
-
-print(result)
-
-print(l)
