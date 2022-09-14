5feb86c0a2d5b2870aacb441c1c61e7a1d2a6ec3
ansehun <63048392+ansehoon1999@users.noreply.github.com>
2022-09-14 11:51:32 +0900

    Update 2178.py

--- 백준 문제 풀이 모음/class 3/2178.py
+++ 백준 문제 풀이 모음/class 3/2178.py
@@ -9,28 +9,28 @@ for _ in range(N) :
     graph.append(list(map(int, sys.stdin.readline().rstrip())))
 
 def bfs(x, y) :
-    dx = [-1, 1, 0, 0]
+
+    dx = [1, -1, 0, 0]
     dy = [0, 0, 1, -1]
 
     queue = deque()
     queue.append((x, y))
-    while queue :
-        x, y = queue.popleft()
 
+
+    while queue :
         for i in range(4) :
             nx = x + dx[i]
             ny = y + dy[i]
-
-            if nx < 0 or nx >= N or ny <0 or ny >= M :
-                continue
         
+            if nx < 0  or nx >= N or ny < 0  or ny >= M :
+                continue
+
             if graph[nx][ny] == 0 :
                 continue
-        
-            if graph[nx][ny] == 1:
+
+            if graph[nx][ny] == 1 :
                 graph[nx][ny] = graph[x][y] + 1
-                queue.append((nx, ny))
+                queue.append((nx, ny)) 
 
     return graph[N-1][M-1]
-
 print(bfs(0,0))
