a0b5807e54737661f5aa03bd717af7ef738e34cf
ansehun <63048392+ansehoon1999@users.noreply.github.com>
2022-09-14 10:12:26 +0900

    Update 11659.py

--- 백준 문제 풀이 모음/class 3/11659.py
+++ 백준 문제 풀이 모음/class 3/11659.py
@@ -6,9 +6,13 @@ for x in range(1, len(num)) :
     num[x] += num[x-1]
 
 
-
 for _ in range(M) :
     i, j = map(int, sys.stdin.readline().split())
-    count = num[j] - num[i]
+    if i == 1 :
+        count = num[j-1] 
+    elif i == j :
+        count = num[j-1] - num[j-2]
+    else :
+        count = num[j-1] - num[i-2]
 
     print(count)
