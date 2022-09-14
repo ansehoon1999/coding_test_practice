d2017e88593e8a6bd83f1970e6959125bd09e93a
ansehun <63048392+ansehoon1999@users.noreply.github.com>
2022-09-14 11:46:51 +0900

    Create 2178.py

---
+++ 백준 문제 풀이 모음/class 3/2178.py
@@ -0,0 +1,36 @@
+
+import sys
+from collections import deque
+
+N, M = map(int, sys.stdin.readline().split())
+graph = []
+
+for _ in range(N) :
+    graph.append(list(map(int, sys.stdin.readline().rstrip())))
+
+def bfs(x, y) :
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, 1, -1]
+
+    queue = deque()
+    queue.append((x, y))
+    while queue :
+        x, y = queue.popleft()
+
+        for i in range(4) :
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or nx >= N or ny <0 or ny >= M :
+                continue
+        
+            if graph[nx][ny] == 0 :
+                continue
+        
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+
+    return graph[N-1][M-1]
+
+print(bfs(0,0))
