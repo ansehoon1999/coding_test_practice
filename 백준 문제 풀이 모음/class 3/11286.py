03f3159a50ea9a65abc768a51b14de04f3ce6ab8
ansehun <63048392+ansehoon1999@users.noreply.github.com>
2022-09-17 14:28:31 +0900

    2022/09/17
    절대값 힙

--- 백준 문제 풀이 모음/class 3/11286.py
+++ 백준 문제 풀이 모음/class 3/11286.py
@@ -3,5 +3,37 @@ import heapq
 
 N = int(sys.stdin.readline())
 
+minus_hp = []
+plus_hp = []
+
 for _ in range(N) :
+    tmp = int(sys.stdin.readline())
+    
+    if tmp < 0 :
+        heapq.heappush(minus_hp, tmp * (-1))
+    elif tmp > 0 :
+        heapq.heappush(plus_hp, tmp) 
+    
+    else :
+        if len(minus_hp) == 0 or len(plus_hp) == 0 :
+            if len(minus_hp) == 0 and len(plus_hp) != 0 :
+                print(heapq.heappop(plus_hp))
+            elif len(minus_hp) != 0 and len(plus_hp) == 0 :
+                print((-1) * heapq.heappop(minus_hp))
+            elif len(minus_hp) == 0 and len(plus_hp) == 0 :
+                print(0)
+                
+        else :
+            if minus_hp[0] == plus_hp[0] :
+                print((-1) * heapq.heappop(minus_hp))
+            elif minus_hp[0] > plus_hp[0] :
+                print(heapq.heappop(plus_hp))
+            elif minus_hp[0] < plus_hp[0]:
+                print((-1) * heapq.heappop(minus_hp))
+
+
+
+
+    
+
     
