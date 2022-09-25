3238663fcac6d1a5c8c6b50800ca012fc18bc86b
ansehun <63048392+ansehoon1999@users.noreply.github.com>
2022-09-25 22:01:12 +0900

    Update 1043.py

--- 백준 문제 풀이 모음/class 4/1043.py
+++ 백준 문제 풀이 모음/class 4/1043.py
@@ -3,9 +3,10 @@ input = sys.stdin.readline
 
 
 def find(parent, x) :
-    if x != parent[x] :
-        parent[x] = parent, parent[x]
+    if parent[x] != x :
+        parent[x] = find(parent, parent[x])
     return parent[x]
+
 
 def union(parent, a, b, know_truth) :
     a = find(parent, a)
@@ -13,24 +14,22 @@ def union(parent, a, b, know_truth) :
 
     if a in know_truth and b in know_truth :
         return
-    
+
     if a in know_truth :
         parent[b] = a
-
     elif b in know_truth :
         parent[a] = b
     
     else :
-        if a  < b :
-            parent[b] = a
-        else :
+        if a > b :
             parent[a] = b
+        else :
+            parent[b] = a 
 
 
 n, m = map(int, input().split())
 
 know_truth = list(map(int, input().split()))[1:]
-
 parent = list(range(n+1))
 
 for _ in range(n) :
@@ -39,4 +38,4 @@ for _ in range(n) :
     party = party_info[1:]
 
     for i in range(party_len - 1) :
-        union(parent, party[i], party[i+1], know_truth) 
+        union(parent, party[i], party[i+1], know_truth)
