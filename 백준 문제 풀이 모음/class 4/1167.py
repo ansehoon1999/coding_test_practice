e194e0de4fae8805ea1c068bd7bac7ca00e88ca0
ansehun <63048392+ansehoon1999@users.noreply.github.com>
2022-09-26 11:10:42 +0900

    Update 1167.py

--- 백준 문제 풀이 모음/class 4/1167.py
+++ 백준 문제 풀이 모음/class 4/1167.py
@@ -4,39 +4,38 @@ sys.setrecursionlimit(10**9)
 
 n = int(input())
 
-adj = []
+adj =[]
 for i in range(n+1) :
     adj.append([])
-
 
 for i in range(n) :
     tmp = list(map(int, input().split()))
 
-    for j in range(1, len(tmp)-1,2) :
+    for j in range(1, len(tmp)-1, 2) :
         adj[tmp[0]].append((tmp[j], tmp[j+1]))
 
-def dfs(x, result) :
 
-    for a, b in adj[x] :
+def dfs(v, result) :
+    
+    for a, b in adj[v] :
         if result[a] == -1 :
-            result[a] = result[x] + b
+            result[a] = result[v] + b
             dfs(a, result)
 
-result1 = [-1] *(n+1)
+result1= [-1] * (n+1)
 result1[1] = 0
 dfs(1, result1)
 
 temp = 0
 index = 0
 
-for i in range(1, len(result1)) :
+for i in range(1, n+1) :
     if temp < result1[i] :
         temp = result1[i]
-        index= i
+        index = i
 
 result2 = [-1] * (n+1)
 result2[index] = 0
 dfs(index, result2)
-
 print(max(result2))
-
+                
