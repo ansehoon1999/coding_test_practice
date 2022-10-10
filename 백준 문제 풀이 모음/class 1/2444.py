c58f7d3d6661b97eec7e35617c455e18a7796ec8
ansehun <63048392+ansehoon1999@users.noreply.github.com>
2022-10-10 22:16:17 +0900

    Create 2444.py

---
+++ 백준 문제 풀이 모음/class 1/2444.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+for i in range(1, n+1) :
+    print(" " * (n-i), end= "")
+    print("*" * ((i-1) * 2 + 1))
+
+for i in range(2, n+1) :
+    print(" " * (i-1), end = "")
+    print("*" * ((n-i) * 2 + 1))
+    
