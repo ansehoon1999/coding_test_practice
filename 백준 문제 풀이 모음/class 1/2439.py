aeb9ee6a698c5813672e31fa7f52ad9fa7bfa81f
ansehun <63048392+ansehoon1999@users.noreply.github.com>
2022-10-10 20:38:31 +0900

    Create 2439.py

---
+++ 백준 문제 풀이 모음/class 1/2439.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+graph = [[" "] * n for i in range(n)]
+
+
+
+for i in range(1, n+1) :
+    for j in range(n-i, n) :
+        graph[i-1][j] = "*"
+
+
+for i in graph :
+    print("".join(i))
+
+
+
