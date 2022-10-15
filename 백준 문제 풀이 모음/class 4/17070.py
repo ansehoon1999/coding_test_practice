306d56fef894efedd1cc4ea0f66e6008703ad9a0
ansehun <63048392+ansehoon1999@users.noreply.github.com>
2022-10-16 00:58:54 +0900

    Update 17070.py

--- 백준 문제 풀이 모음/class 4/17070.py
+++ 백준 문제 풀이 모음/class 4/17070.py
@@ -1,24 +1,26 @@
 import sys
 input = sys.stdin.readline
-n = int(input())
-
-graph = [list(map(int, input().split())) for _ in range(n)]
-graph[0][0] = -1
-graph[0][1] = -1
-
-from collections import deque
-def bfs() :
-
-    queue = deque()    
-    queue.append((0,1))
-
-    while queue :
-        node_x, node_y = queue.popleft()
-
-        
 
 
-for i in range(n) :
-    for j in range(n) :
-        print(graph[i][j], end= " ")
-    print()
+
+def solution() :
+    dp[0][0][1] = 1
+
+    for i in range(2, N) :
+        if board[0][i] == 0 :
+            dp[0][0][i] = dp[0][0][i-1]
+    
+    for r in range(1, N) :
+        for c in range(1, N) :
+            if board[r][c] == 0 and board[r][c-1] == 0 and board[r-1][c] == 0 :
+                dp[1][r][c] = dp[0][r-1][c-1] + dp[1][r-1][c-1] + dp[2][r-1][c-1]
+            if board[r][c] == 0 :
+                dp[0][r][c] = dp[0][r][c-1] + dp[1][r][c-1]
+                dp[2][r][c] = dp[2][r-1][c] + dp[1][r-1][c]
+
+    print(sum(dp[i][N-1][N-1] for i in range(3)))
+
+N = int(input())
+board = [list(map(int, input().split())) for _ in range(N)]
+dp = [[[0 for _ in range(N)] for _ in range(N)] for _ in range(3)]
+solution()
