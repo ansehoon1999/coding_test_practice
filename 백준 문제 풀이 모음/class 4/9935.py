5b0cab7607551e41654b73f51d755ebcbfb9d3b1
ansehun <63048392+ansehoon1999@users.noreply.github.com>
2022-10-17 18:09:40 +0900

    Update 9935.py

--- 백준 문제 풀이 모음/class 4/9935.py
+++ 백준 문제 풀이 모음/class 4/9935.py
@@ -4,29 +4,24 @@ input = sys.stdin.readline
 class stack :
     def __init__(self) :
         self.top = []
-    def __str__(self) :
-        return str(self.top)
-    def push(self, item) :
-        self.top.append(item)
+    def push(self, data) :
+        self.top.append(data)
     def pop(self) :
-        if not self.isEmpty() :
-            return self.top.pop(-1)
-        else :
-            print('dd')
-            exit()
-    def isEmpty(self) :
-        return len(self.top) == 0
+        self.top.pop(-1)
 
-arr = list(input().rstrip())
-target = input().rstrip()
 stk = stack()
+
+arr = input().rstrip()
+target = input().rstrip()
+l = len(target)
+
 for i in arr :
     stk.push(i)
-    if ''.join(stk.top[-len(target):]) == target :
-        for _ in range(len(target)) :
+    if ''.join(stk.top[- l :]) == target :
+        for _ in range(l) :
             stk.pop()
 
 if stk.top :
     print(''.join(stk.top))
 else :
-    print('FRULA') 
+    print('FRULA')
