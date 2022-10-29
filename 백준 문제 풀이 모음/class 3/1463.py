fc00924810814ad695ac62a3046754b93471dc0f
ansehun <63048392+ansehoon1999@users.noreply.github.com>
2022-10-29 12:27:34 +0900

    2022/10/29

--- 백준 문제 풀이 모음/class 3/1463.py
+++ 백준 문제 풀이 모음/class 3/1463.py
@@ -1,16 +1,41 @@
-import sys
+# import sys
 
-n = int(sys.stdin.readline())
+# n = int(sys.stdin.readline())
 
-count = {1: 0 , 2: 1}
+# count = {1: 0 , 2: 1}
 
-def make(n) :
-    if n in count :
-        return count[n]
+# def make(n) :
+#     if n in count :
+#         return count[n]
     
-    cnt =  1 + min (make(n//3) + n %3, make(n//2) + n % 2)
-    count[n] = cnt
-    return cnt
+#     cnt =  1 + min (make(n//3) + n %3, make(n//2) + n % 2)
+#     count[n] = cnt
+#     return cnt
     
-print(make(n))
+# print(make(n))
 
+n = int(input())
+d = [0] * (n + 1)
+before = [1] * (n + 1)	
+for i in range(2, n + 1):
+    d[i] = d[i - 1] + 1
+    before[i] = i-1 
+    if i % 3 == 0:
+        d[i] = min(d[i], d[i // 3] + 1)	
+        if d[i] == d[i//3] + 1 :
+            before[i] = i // 3
+    if i % 2 == 0:
+        d[i] = min(d[i], d[i // 2] + 1)
+        if d[i] == d[i//2] + 1 :
+            before[i] = i // 2
+
+
+print(d[n])
+i = n 
+print(i, end = " ")
+while True :
+    if i == 1 :
+        break
+
+    i = before[i]
+    print(i, end = " ")
---
+++ 백준 문제 풀이 모음/class 5/12852.py
@@ -0,0 +1,11 @@
+
+
+import sys
+input = sys.stdin.readline
+
+num = int(input())
+
+arr = [0] * (num+1)
+arr[num] = 1
+ 
+print(arr)
