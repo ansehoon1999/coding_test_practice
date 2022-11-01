9f609cfbb7de172e50d626fb535f32036d3aea97
ansehun <63048392+ansehoon1999@users.noreply.github.com>
2022-11-02 00:35:16 +0900

    2022/11/01

--- 백준 문제 풀이 모음/class 5/1197.py
+++ 백준 문제 풀이 모음/class 5/1197.py
@@ -1,70 +1,82 @@
 
-# from sys import stdin
+from sys import stdin
 
-# read = stdin.readline
-# V, S = map(int, read().split())
+read = stdin.readline
+V, S = map(int, read().split())
 
-# edge = []
-# for _ in range(S) :
-#     a, b, w = map(int, read().split())
-#     edge.append((w,a,b))
+edge = []
+for _ in range(S) :
+    a, b, w = map(int, read().split())
+    edge.append((w,a,b))
 
-# edge.sort(key = lambda x : x[0])
-# parent = list(range(V+1))
-# sum = 0
+edge.sort(key = lambda x : x[0])
+parent = list(range(V+1))
+sum = 0
 
-# def union(a, b) :
-#     a = find(a)
-#     b = find(b)
+edge.sort(key = lambda x : x[0])
+parent = list(range(V+1))
+sum = 0
 
-#     if a < b :
-#         parent[b] = a
-#     else :
-#         parent[a] = b
+def union(a, b) :
+    a = find(a)
+    b = find(b)
+
+    if a < b :
+        parent[b] = a
+    else :
+        parent[a] = b
+
+def find(s) :
+    if s == parent[s] :
+        return s
+
+    parent[s] = find(parent[s])
+    return parent[s]
+
+for w, a, b in edge :
+    if find(a) != find(b) :
+        union(a, b)
+        sum += w
+
+print(sum)
 
 
-# def find(a) :
-#     if a == parent[a] :
-#         return a
-#     parent[a] = find(parent[a])
-#     return parent[a]
 
-# for w, s, e  in edge :
-#     if find(s) != find(e) :
-#         union(s,e)
-#         sum += w
 
-# print(sum)
 
-import heapq
-import collections
-import sys
-sys.setrecursionlimit(10 ** 6)
-input = sys.stdin.readline
 
-n, m = map(int, input().split())
-graph = collections.defaultdict(list)
-visited = [0] * (n+1)
+# import heapq
+# import collections
+# import sys
+# sys.setrecursionlimit(10 ** 6)
+# input = sys.stdin.readline
 
-for i in range(m) :
-    u, v, weight = map(int, input().split())
-    graph[u].append([weight, u, v])
-    graph[v].append([weight, v, u])
+# n, m = map(int, input().split())
+# graph = collections.defaultdict(list)
+# visited = [0] * (n + 1)
 
-def prim(graph, start_node) :
-    visited[start_node] = 1
-    candidate = graph[start_node]
-    heapq.heapify(candidate)
-    total_weight = 0
+# for _ in range(m) :
+#     u, v, weight = map(int, input().split())
+#     graph[u].append([weight, u, v])
+#     graph[v].append([weight, v, u])
 
-    while candidate :
-        weight, u, v = heapq.heappop(candidate)
-        if visited[v] == 0 :
-            visited[v] = 1
-            total_weight += weight
+# def prim(graph, start_node) :
+#     visited[start_node] = 1
+#     candidate = graph[start_node]
+#     heapq.heapify(candidate)
+    
+#     total_weight = 0
 
-            for edge in graph[v] :
-                if visited[edge[2]] == 0 :
-                    heapq.heappush(candidate, edge)
-    return total_weight
-print(prim(graph, 1))
+#     while candidate :
+#         weight, u, v = heapq.heappop(candidate)
+#         if visited[v] == 0 :
+#             visited[v] = 1
+#             total_weight += weight
+
+#             for edge in graph[v] :
+#                 if visited[edge[2]] == 0 :
+#                     heapq.heappush(candidate, edge)
+#     return total_weight
+
+# print(prim(graph, 1))
+
