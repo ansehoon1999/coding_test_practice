82b363c793b4d185aea9cf10a2c82c0de90119ac
ansehun <63048392+ansehoon1999@users.noreply.github.com>
2022-11-05 00:44:56 +0900

    Update 4386.py

--- 백준 문제 풀이 모음/class 5/4386.py
+++ 백준 문제 풀이 모음/class 5/4386.py
@@ -1,21 +1,22 @@
 import sys
-import math
 input = sys.stdin.readline
 
 n = int(input())
 
 stars = []
 edges = []
+sum = 0
 parents = list(range(n+1))
-
 for _ in range(n) :
     x, y = map(float, input().split())
     stars.append((x, y))
 
-for i in range(n-1) :
+for i in range(0, n-1) :
     for j in range(i+1, n) :
-        edges.append((math.sqrt((stars[i][0] - stars[j][0])**2 + (stars[i][1] - stars[j][1])**2), i, j)) 
+        edges.append ( ((((stars[i][0] - stars[j][0]) ** 2) +   ((stars[i][1] - stars[j][1]) ** 2)) ** (1/2) , i, j) )
+
 edges.sort()
+
 def union(a, b) :
     a = find(a)
     b = find(b)
@@ -23,19 +24,18 @@ def union(a, b) :
     if a < b :
         parents[b] = a
     else :
-        parents[a] = b        
+        parents[a] = b
 
 def find (x) :
-    if x == parents[x]  :
+    if x == parents[x] :
         return x
     
     parents[x] = find(parents[x])
     return parents[x]
 
-sum = 0
 for w, a, b in edges :
     if find(a) != find(b) :
         union(a, b)
-        sum = sum + w
+        sum += w
 
-print(round(sum,2))
+print(round(sum, 2))
