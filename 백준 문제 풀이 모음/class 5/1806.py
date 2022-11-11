1cb9d842e9b9dc16c5bd0f03b75c3cdeda2a82c8
ansehun <63048392+ansehoon1999@users.noreply.github.com>
2022-11-11 11:46:28 +0900

    2022/11/11

--- 백준 문제 풀이 모음/class 5/1806.py
+++ 백준 문제 풀이 모음/class 5/1806.py
@@ -10,17 +10,31 @@ interval_sum = 0
 
 end = 0
 minn = 10000000
+
 for start in range(N) :
     while interval_sum < S and end < N :
         interval_sum += arr[end]
         end += 1
 
     if interval_sum >= S :
-        if minn > (end - start) :
-            minn = (end-start)
-    interval_sum  -= arr[start]
-
-if  minn == 10000000 :
+        if minn > (end-start) :
+            minn = end-start
+    interval_sum -= arr[start]
+if minn == 10000000 :
     print(0)
 else :
     print(minn)
+# for start in range(N) :
+#     while interval_sum < S and end < N :
+#         interval_sum += arr[end]
+#         end += 1
+
+#     if interval_sum >= S :
+#         if minn > (end - start) :
+#             minn = (end-start)
+#     interval_sum  -= arr[start]
+
+# if  minn == 10000000 :
+#     print(0)
+# else :
+#     print(minn)
