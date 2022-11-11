8242c9bdece6ca7e8b02b746baebe2b0e1cb9640
ansehun <63048392+ansehoon1999@users.noreply.github.com>
2022-11-11 10:32:22 +0900

    Create 1806.py

---
+++ 백준 문제 풀이 모음/class 5/1806.py
@@ -0,0 +1,26 @@
+
+
+import sys
+input = sys.stdin.readline
+
+N, S = map(int, input().split())
+arr = list(map(int, input().split()))
+
+interval_sum = 0
+
+end = 0
+minn = 10000000
+for start in range(N) :
+    while interval_sum < S and end < N :
+        interval_sum += arr[end]
+        end += 1
+
+    if interval_sum >= S :
+        if minn > (end - start) :
+            minn = (end-start)
+    interval_sum  -= arr[start]
+
+if  minn == 10000000 :
+    print(0)
+else :
+    print(minn)
