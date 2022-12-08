5893f21ebd200c8904e24a87a8467f53a449c0c5
ansehun <63048392+ansehoon1999@users.noreply.github.com>
2022-12-08 10:45:19 +0900

    Create 11651.kt

---
+++ 백준 문제 풀이 모음/kotlin/class 2/11651.kt
@@ -0,0 +1,32 @@
+import java.util.StringTokenizer
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val n = readLine().toInt()
+
+    val arr = Array<Pair<Int, Int>>(n) { Pair(0,0) }
+    for (i in 0 until n) {
+        val input = StringTokenizer(readLine())
+
+        val x = input.nextToken().toInt()
+        val y = input.nextToken().toInt()
+
+        arr.set(i, Pair(x, y))
+    }
+
+    arr.sortBy {
+        it.second
+    }
+
+    arr.sortBy {
+        it.first
+    }
+
+    val sb = StringBuilder()
+    arr.forEach {
+        sb.append(it.first.toString() + " " + it.second.toString())
+        sb.append('\n')
+    }
+
+    println(sb.toString())
+
+}
