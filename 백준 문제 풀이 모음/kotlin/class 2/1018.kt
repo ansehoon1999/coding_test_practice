19994b989b5189f013d1214f3831d183a28734b0
ansehun <63048392+ansehoon1999@users.noreply.github.com>
2022-12-10 10:39:50 +0900

    Update 1018.kt

--- 백준 문제 풀이 모음/kotlin/class 2/1018.kt
+++ 백준 문제 풀이 모음/kotlin/class 2/1018.kt
@@ -1,140 +1,85 @@
 import java.util.*
 
+
+var min = 100000000
+
 val bw = charArrayOf('B','W','B','W','B','W','B','W')
 val wb = charArrayOf('W','B','W','B','W','B','W','B')
-var matrix : Array<CharArray> = arrayOf()
-var nm: List<Int> = listOf()
 
-fun check(x: Int, y: Int): Int {
+fun check(x : Int, y : Int, matrix : Array<CharArray>) : Int {
 
-    var cntW = 0
-    var cntB = 0
+    var bw_count = 0
+    var wb_count = 0
 
-    for (i in x until x + 8) {
-
-        when(i%2) {
-            0 -> {
-                for (j in y until y + 8) {
-                    if (bw[j - y] != matrix[i][j])
-                        cntB++
-                    if (wb[j - y] != matrix[i][j])
-                        cntW++
-                }
-            }
-            1 -> {
-                for (j in y until y + 8) {
-                    if (bw[j - y] != matrix[i][j])
-                        cntW++
-                    if (wb[j - y] != matrix[i][j])
-                        cntB++
-                }
-            }
-        }
-    }
-
-    return Math.min(cntW, cntB)
-
-}
-fun main()= with(System.`in`.bufferedReader()) {
-
-
-    val str = StringTokenizer(readLine())
-    val row = str.nextToken().toInt()
-    val col = str.nextToken().toInt()
-
-    matrix = Array(row) { IntArray(col) }
-        .map { readLine().toCharArray() }
-        .toTypedArray()
-
-    for (i in 0 until 8) {
-        for ( j in 0 until 8) {
-            print(matrix[i][j])
-        }
-        println()
-    }
-
-    var low = row * col
-
-    for (i in 0 until row - 7) {
-        for ( j in 0 until col - 7 ) {
-            low = Math.min(low, check(i, j))
-        }
-    }
-
-
-    println(low)
-}
-
-//import java.io.BufferedReader
-//import java.io.InputStreamReader
-//
-//var nm: List<Int> = listOf()
-//var matrix: Array<CharArray> = arrayOf()
-//val bw = charArrayOf('B','W','B','W','B','W','B','W')
-//val wb = charArrayOf('W','B','W','B','W','B','W','B')
-//
-//fun check(x: Int, y: Int): Int {
-//
-//    var cntW = 0
-//    var cntB = 0
-//
 //    for (i in x until x + 8) {
-//
-//        when(i%2) {
-//            0 -> {
-//                for (j in y until y + 8) {
-//                    if (bw[j - y] != matrix[i][j])
-//                        cntB++
-//                    if (wb[j - y] != matrix[i][j])
-//                        cntW++
-//                }
-//            }
-//            1 -> {
-//                for (j in y until y + 8) {
-//                    if (bw[j - y] != matrix[i][j])
-//                        cntW++
-//                    if (wb[j - y] != matrix[i][j])
-//                        cntB++
-//                }
-//            }
-//        }
-//    }
-//
-//    return Math.min(cntW, cntB)
-//
-//}
-//fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
-//
-//    nm = readLine()
-//        .split(" ")
-//        .map { it.toInt() }
-//    matrix = Array(nm[0]) { IntArray(nm[1]) }
-//        .map { readLine().toCharArray() }
-//        .toTypedArray()
-//
-//    for (i in 0 until 8) {
-//        for ( j in 0 until 8) {
+//        for (j in y until y + 8) {
 //            print(matrix[i][j])
+//            print(' ')
 //        }
 //        println()
 //    }
-//
-//    var xShift = 0
-//    var yShift = 0
-//    var min = nm[0] * nm[1]
-//
-//    while (true) {
-//        if (yShift + 8 > nm[1]) break
-//
-//        min = Math.min(min, check(xShift, yShift))
-//
-//        if (xShift + 8 < nm[0]) xShift++
-//        else {
-//            xShift = 0
-//            yShift++
+
+    for (i in x until x + 8) {
+        for (j in y until y+8) {
+            if (i % 2 == 0) {
+                if (!matrix[i][j].equals(bw[j-y])) {
+                    bw_count ++
+                }
+
+                if (!matrix[i][j].equals(wb[j-y])) {
+                    wb_count ++
+                }
+            } else {
+                if (!matrix[i][j].equals(wb[j-y])) {
+                    bw_count ++
+                }
+
+                if (!matrix[i][j].equals(bw[j-y])) {
+                    wb_count ++
+                }
+            }
+        }
+    }
+
+//    println(wb_count)
+//    println(bw_count)
+    return Math.min(wb_count, bw_count)
+
+}
+
+
+fun main()= with(System.`in`.bufferedReader()) {
+    val str = readLine().split(' ').toList()
+    val n = str[0].toInt()
+    val m = str[1].toInt()
+    val matrix = Array(n) { CharArray(m) {'0'} }
+
+
+    for (i in 0 until n) {
+        val input = readLine().toCharArray()
+        for ( j in 0 until m) {
+            matrix[i][j] = input[j]
+        }
+    }
+
+    for (i in 0 .. n-8) {
+        for (j in 0 .. m-8) {
+
+            min = Math.min(min, check(i, j, matrix))
+        }
+    }
+
+    println(min)
+//    val sb = StringBuilder()
+//    matrix.forEach {
+//        it.forEach {
+//            sb.append(it)
+//            sb.append(' ')
 //        }
+//        sb.append('\n')
 //    }
 //
-//    println(min)
-//
-//}
+//    println(sb.toString())
+
+
+}
