2e4877ba0a5e3770533d8ceb32cdba4eb7c3eb9b
ansehun <63048392+ansehoon1999@users.noreply.github.com>
2022-12-10 10:40:00 +0900

    Update 1654.kt

--- 백준 문제 풀이 모음/kotlin/class 2/1654.kt
+++ 백준 문제 풀이 모음/kotlin/class 2/1654.kt
@@ -1,41 +1,84 @@
-import java.util.StringTokenizer
-
-
 
 fun main () = with(System.`in`.bufferedReader()) {
-    val str = StringTokenizer(readLine())
+    val str = readLine().split(' ').toList()
 
-    val k = str.nextToken().toInt()
-    val n = str.nextToken().toInt()
-
+    val n = str[0].toInt()
+    val m = str[1].toInt()
 
     val list = mutableListOf<Long>()
-    var high :Long = -1
-
-    for (i in 0 until k) {
-        val input = readLine().toLong()
-        list.add(input)
-        high = Math.max(high, input)
+    var max_length : Long = -1
+    repeat(n) {
+        val input = readLine()
+        list.add(input.toLong())
+        max_length = Math.max(max_length, input.toLong())
     }
 
+//    var sum = 0
+//    list.forEach{
+//        sum += (it )
+//    }
+//
     var start : Long = 1L
-    var end : Long = high
+    var end : Long= max_length
 
-    while (start <= end) {
-        var mid : Long = (start + end) / 2
-        var count :Long = 0L
-
-        for (i in 0 until k) {
-            count += (list.get(i) / mid)
+    while(start <= end) {
+        val mid : Long = (start + end) / 2
+        var sum : Long = 0
+        list.forEach {
+            sum += (it / mid)
         }
 
-        if (count >= n) {
+        if (sum >= m) {
             start = mid + 1
         } else {
             end = mid - 1
         }
+
+//        println("${start} ${end}")
+
     }
 
+    println(end)
 
-    print(end.toInt())
 }
+
+
+
+
+//import java.util.*
+//
+//fun main () = with(System.`in`.bufferedReader()) {
+//    val str = StringTokenizer(readLine())
+//
+//    val k = str.nextToken().toInt()
+//    val n = str.nextToken().toInt()
+//
+//    val list = mutableListOf<Long>()
+//    var high :Long = -1
+//
+//    for (i in 0 until k) {
+//        val input = readLine().toLong()
+//        list.add(input)
+//        high = Math.max(high, input)
+//    }
+//
+//    var start : Long = 1L
+//    var end : Long = high
+//
+//    while (start <= end) {
+//        var mid : Long = (start + end) / 2
+//        var count :Long = 0L
+//
+//        for (i in 0 until k) {
+//            count += (list.get(i) / mid)
+//        }
+//
+//        if (count >= n) {
+//            start = mid + 1
+//        } else {
+//            end = mid - 1
+//        }
+//    }
+//
+//    print(end.toInt())
+//}
