6b1e6515c4fece648a11f038da741f2c5645e648
ansehun <63048392+ansehoon1999@users.noreply.github.com>
2022-12-10 10:57:55 +0900

    Update 1929.kt

--- 백준 문제 풀이 모음/kotlin/class 2/1929.kt
+++ 백준 문제 풀이 모음/kotlin/class 2/1929.kt
@@ -1,32 +1,33 @@
-import java.lang.StringBuilder
-import java.util.StringTokenizer
 
 fun main () = with(System.`in`.bufferedReader()) {
-    val str = StringTokenizer(readLine())
-    val m = str.nextToken().toInt()
-    val n = str.nextToken().toInt()
-    var cnt = 0
+    val str = readLine().split(' ').toList()
+    val m = str[0].toInt()
+    val n = str[1].toInt()
 
-    for (i in m .. n) {
-        if (i == 1) {
-            continue
-        }
+    val arr = Array<Boolean> (n+1) { false }
 
-        var j = 2
-        while (j <= Math.sqrt(i.toDouble())) {
-            if (i % j == 0) {
-                cnt ++
-                break
+    arr[0] = true
+    arr[1] = true
+
+    for (i in 2 .. (Math.sqrt((n+1).toDouble())).toInt()) {
+        if (arr[i]) continue
+
+        for (j in i*i until  n+1 step i) {
+            if (!arr[j]) {
+                arr[j] = true
             }
-            j ++
         }
-
-        if (cnt == 0) {
-            println(i)
-        }
-
-        cnt = 0
     }
 
+    val sb = StringBuilder()
+    for (i in m .. n) {
+        if(!arr[i]) {
+            sb.append(i)
+            sb.append(' ')
+        }
+    }
+
+    print(sb)
 
 }
+
