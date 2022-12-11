2efbef4c41c80228f5d5adf828f0a8d5435c2ff7
ansehun <63048392+ansehoon1999@users.noreply.github.com>
2022-12-12 00:19:20 +0900

    Create 1107.kt

---
+++ 백준 문제 풀이 모음/kotlin/class 3/1107.kt
@@ -0,0 +1,66 @@
+package class3
+
+val btn : MutableList<String> = mutableListOf()
+var count = 1
+val can = mutableListOf<String>()
+fun main () = with(System.`in`.bufferedReader()) {
+    val n = readLine().toInt()
+
+    val count = readLine().toInt()
+    if (count == 0) {
+        print(n.toString().toCharArray().size)
+    } else {
+    val noBtn = readLine().split(' ').toList()
+
+    for (i in 0 .. 9) {
+        if (!noBtn.contains(i.toString())) {
+            btn.add(i.toString())
+        }
+    }
+
+//    for (i in 1 .. btn.size) {
+//        dubPermutation(i)
+//    }
+
+        for (i in 1..7) {
+            dubPermutation(0, i)
+        }
+
+        var min = Math.abs(100 - n)
+
+        can.forEach {
+//        println(it)
+            min = Math.min(min, Math.abs(n - it.toInt()) + it.toCharArray().size)
+        }
+
+        print(min)
+    }
+
+}
+
+val pickednum = mutableListOf<String>()
+
+fun dubPermutation(cnt : Int ,depth : Int) {
+    if (cnt == depth) {
+        var result = ""
+        pickednum.forEach {
+            result = result + it
+        }
+
+        can.add(result)
+        return
+    }
+
+    for (index in 0 until btn.size) {
+        pickednum.add(btn[index])
+        dubPermutation(cnt + 1, depth)
+        pickednum.removeAt(pickednum.lastIndex)
+    }
+
+}
+
+
+
+
+
+
