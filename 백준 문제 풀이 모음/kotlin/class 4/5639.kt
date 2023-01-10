b83dd35a751c5fcc5dfbe90572e6367f2fcb5c96
ansehun <63048392+ansehoon1999@users.noreply.github.com>
2023-01-10 17:44:04 +0900

    Update 5639.kt

--- 백준 문제 풀이 모음/kotlin/class 4/5639.kt
+++ 백준 문제 풀이 모음/kotlin/class 4/5639.kt
@@ -1,40 +1,52 @@
 
 
-data class Node (val root : Int, var left : Node?, var right : Node?) {
-  fun addChildeNode(n :Int) {
-      if (n > root) {
-          if (right == null) right = Node(n, null, null)
-          else right!!.addChildeNode(n)
-      } else {
-          if (left == null) left = Node(n,null, null)
-          else left!!.addChildeNode(n)
-      }
-  }
-}
 
-val sb = StringBuilder()
 
-fun main() = with(System.`in`.bufferedReader()){
+data class TreeNode <T> (
+    var data : Int,
+    var left : TreeNode <T>? = null,
+    var right : TreeNode <T>? = null
+)
 
-    val root = Node(readLine().toInt(), null, null)
+class Tree {
+    var root : TreeNode<Int>? = null
 
-    while (true) {
-        val input = readLine()
-        if (input == null) break
-        else {
-            root.addChildeNode(input.toInt())
+    fun add (data : Int) {
+        if (root == null) {
+            root = TreeNode(data)
+        } else {
+            search(root!!, data)
         }
     }
 
-    postOrder(root)
-    println(sb)
+    private fun search(root: TreeNode<Int>, data: Int) {
+        if (root.data > data) {
+            if (root.left == null) root.left = TreeNode(data)
+            else search(root.left!!, data)
+        } else {
+            if (root.right == null) root.right = TreeNode(data)
+            else search(root.right!!, data)
+        }
+    }
+
 }
 
-fun postOrder(node: Node?) {
-    if (node != null) {
-        postOrder(node.left)
-        postOrder(node.right)
-        sb.append(node.root)
-        sb.append('\n')
-    }
+fun postOrder (root : TreeNode<Int>) {
+    if (root.left != null) postOrder(root.left!!)
+    if (root.right != null) postOrder(root.right!!)
+    println(root.data)
 }
+fun main() {
+    val sb = System.`in`.bufferedReader()
+    val tree = Tree()
+    while (true) {
+        val n = sb.readLine()
+        if (n == null) break
+        else {
+            tree.add(n.toInt())
+        }
+    }
+
+    postOrder(tree.root!!)
+}
+
