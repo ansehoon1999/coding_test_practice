fc99fd410bc0b2a37c5d37fa5276879ec07b44ad
ansehun <63048392+ansehoon1999@users.noreply.github.com>
2023-01-22 17:22:49 +0900

    Update 55.kt

--- leetcode/medium/55.kt
+++ leetcode/medium/55.kt
@@ -1,14 +1,22 @@
 class Solution {
     fun canJump(nums: IntArray): Boolean {
+        val list = MutableList<Boolean>(nums.size) { false }
+        list[0] = true
 
-        var boundary = 1
-        nums.forEach {
-            if (boundary == 0) return false
+        for (i in 1 until nums.size) {
+            for (j in i-1 downTo 0) {
+                if (list[j] && (j+nums[j]) >= i) {
+                    // println("nums ${nums[j]} j ${j}")
+                    list[i] = true
+                    break
+                }      
+            }
 
-            boundary = Math.max(boundary-1, it)
         }
-        return true
 
+        // println(list)
+        
+        return list.last()
     }
 
     
