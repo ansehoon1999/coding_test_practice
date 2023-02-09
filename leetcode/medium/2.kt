b7d228e4fa8bb90f611220473e149016b6acf21f
ansehun <63048392+ansehoon1999@users.noreply.github.com>
2023-02-09 10:38:57 +0900

    Update 2.kt

--- leetcode/medium/2.kt
+++ leetcode/medium/2.kt
@@ -7,31 +7,45 @@
  *     var next: ListNode? = null
  * }
  */
+
+
+
 class Solution {
+
+    fun add (l1: ListNode?, l2: ListNode?) : Int {
+        return (l1?.`val` ?: 0) + (l2?.`val` ?: 0)
+    }
     fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
         
-        var list1 = l1
-        var list2 = l2
-
-        val head = ListNode(0)
-        var result : ListNode? = head
+        var curLeft = l1
+        var curRight = l2
+        var curr : ListNode? = null
+        var root : ListNode? = null
+        
         var carry = 0
-
-        while (list1 != null || list2 != null || carry > 0) {
+        
+        while(curLeft != null || curRight != null || carry > 0) {
             
-            val x1 = list1?.`val` ?: 0
-            val x2 = list2?.`val` ?: 0
-            val sum = (x1 + x2 + carry) % 10
-            carry = (x1 + x2 + carry) / 10
-
-            result?.next = ListNode(sum)
-            result = result?.next
-
-            if (list1 != null) list1 = list1.next
-            if (list2 != null) list2 = list2.next
-
+            val added = add(curLeft, curRight) + carry
+            
+            carry = if (added > 9) 1 else 0
+            
+            val node = ListNode(added % 10)
+            
+            if (root == null) {
+                root = node
+                curr = node
+            } else {
+                curr?.next = node
+                curr = node
+            }
+            
+            curLeft = curLeft?.next
+            curRight = curRight?.next
+            
         }
-
-        return head.next
+        
+        return root
+        
     }
 }
