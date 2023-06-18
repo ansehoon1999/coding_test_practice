ac700c5f19e11084ce52dac3b0cce4fb08e8d780
안세훈 <ansehoon1999@gmail.com>
2023-06-18 22:06:45 +0900

    dfs/bfs: 단어 변환

---
+++ 프로그래머스 풀이 모음/고득점 Kit/깊이:너비 우선 탐색/깊이너비우선탐색4.kt
@@ -0,0 +1,43 @@
+class Solution {
+
+    lateinit var visited: MutableList<Boolean>
+    var result = 10000000
+
+    fun solution(begin: String, target: String, words: Array<String>): Int {
+
+        visited = MutableList<Boolean>(words.size) { false }
+
+        if (target !in words) {
+            return 0
+        }
+
+        dfs(begin, target, words, 0)
+        return result
+    }
+
+    fun dfs(begin: String, target: String, words: Array<String>, count: Int) {
+
+        if(begin == target) {
+            result = minOf(result, count)
+            return
+        }
+
+        for(i in 0 until words.size) {
+            var diffSum = 0
+
+            for(j in 0 until begin.length) {
+                if(begin[j] != words[i][j]) {
+                    diffSum ++
+                }
+            }
+
+            if(diffSum == 1 && !visited[i]) {
+                visited[i] = true
+                dfs(words[i], target, words, count+1)
+                visited[i] = false
+            }
+        }
+
+    }
+
+}
