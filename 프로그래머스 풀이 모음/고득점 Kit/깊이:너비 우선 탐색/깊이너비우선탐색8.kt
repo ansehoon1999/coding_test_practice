d041fa75e5561cacf8b9a3c22b762d4ee423069c
안세훈 <ansehoon1999@gmail.com>
2023-06-27 20:14:44 +0900

    dfs: 퍼즐조각 채우기

--- 프로그래머스 풀이 모음/고득점 Kit/깊이:너비 우선 탐색/깊이너비우선탐색8.kt
+++ 프로그래머스 풀이 모음/고득점 Kit/깊이:너비 우선 탐색/깊이너비우선탐색8.kt
@@ -1,263 +1,226 @@
-import java.util.*
-import kotlin.collections.ArrayList
+class Solution {
 
-fun main() {
 
-    val game_board = arrayOf(
-            intArrayOf(1,1,0,0,1,0),
-            intArrayOf(0,0,1,0,1,0),
-            intArrayOf(0,1,1,0,0,1),
-            intArrayOf(1,1,0,1,1,1),
-            intArrayOf(1,0,0,0,1,0),
-            intArrayOf(0,1,1,1,0,0),
+    var xLength = 0
+    var yLength = 0
+    lateinit var usedPuzzle: MutableList<Boolean>
+    var puzzleList = ArrayList<MutableList<MutableList<Int>>>()
+    var spaceList = ArrayList<MutableList<MutableList<Int>>>()
+    var result = 0
+
+    var direction = mutableListOf<MutableList<Int>>(
+            mutableListOf(1, 0),
+            mutableListOf(-1, 0),
+            mutableListOf(0, 1),
+            mutableListOf(0, -1)
     )
 
-    val table = arrayOf(
-            intArrayOf(1,0,0,1,1,0),
-            intArrayOf(1,0,1,0,1,0),
-            intArrayOf(0,1,1,0,1,1),
-            intArrayOf(0,0,1,0,0,0),
-            intArrayOf(1,1,0,1,1,0),
-            intArrayOf(0,1,0,0,0,0),
-    )
 
-    println(solution(game_board, table))
-}
+    fun solution(gameBoard: Array<IntArray>, table: Array<IntArray>): Int {
 
-var yLength = 0
-var xLength = 0
-var answer = 0
-lateinit var usedPuzzle: MutableList<Boolean>
-val puzzleList = ArrayList<MutableList<MutableList<Int>>>()
-val spaceList = ArrayList<MutableList<MutableList<Int>>>()
-val direction = mutableListOf<MutableList<Int>>(
-        mutableListOf(-1, 0),
-        mutableListOf(0, 1),
-        mutableListOf(1, 0),
-        mutableListOf(0, -1)
-)
+        yLength = gameBoard.size
+        xLength = gameBoard[0].size
 
-
-fun solution(gameBoard: Array<IntArray>, table: Array<IntArray>): Int {
-
-    yLength = gameBoard.size
-    xLength = gameBoard[0].size
-
-    var visited = MutableList<MutableList<Boolean>>(yLength) {
-        MutableList<Boolean>(xLength) { false }
-    }
-
-    for(y in 0 until yLength) {
-        for(x in 0 until xLength) {
-            if(visited[y][x] || table[y][x] == 0) {
-                continue
-            }
-            puzzleList.add(makePuzzlePiece(table, visited, y, x))
-        }
-    }
-
-    usedPuzzle = MutableList<Boolean>(puzzleList.size) { false }
-    visited = MutableList<MutableList<Boolean>>(yLength) {
-        MutableList<Boolean>(xLength) { false }
-    }
-    for(y in 0 until yLength) {
-        for(x in 0 until xLength) {
-            if(visited[y][x] || gameBoard[y][x] == 1) continue
-
-            spaceList.add(makeSpacePiece(gameBoard, visited, y, x))
-        }
-    }
-
-
-    puzzleList.forEach {
-        it.forEach {
-            println(it)
-        }
-        println()
-    }
-
-    println()
-
-    spaceList.forEach {
-        it.forEach {
-            println(it)
-        }
-        println()
-    }
-
-    backtracking(0, 0)
-
-    return answer
-}
-
-fun backtracking(idx: Int, sum: Int) {
-
-    answer = answer.coerceAtLeast(sum)
-    if(idx == spaceList.size) {
-        return
-    }
-
-    for(i in puzzleList.indices) {
-        if(usedPuzzle[i]) {
-            continue
+        var visited = MutableList<MutableList<Boolean>>(yLength) {
+            MutableList<Boolean>(xLength) { false }
         }
 
-        for(j in 0 until 4) {
-            val matchCnt = canMatchBoard(spaceList[idx], i)
+        for(y in 0 until yLength) {
+            for(x in 0 until xLength) {
+                if(visited[y][x] || gameBoard[y][x] == 1) continue
 
-            if(matchCnt == 0 ) {
-                puzzleList[i] = rotate(i)
-            } else {
-                usedPuzzle[i] = true
-                backtracking(idx+1, sum + matchCnt)
-
-                break
+                spaceList.add(makeSpacePiece(y, x, visited, gameBoard))
             }
         }
-    }
 
-    backtracking(idx+1, sum)
-}
-
-fun canMatchBoard(space: MutableList<MutableList<Int>>, idx: Int): Int {
-    val puzzle = puzzleList[idx]
-
-    if(space.size != puzzle.size || space[0].size != puzzle[0].size) {
-        return 0
-    }
-
-    var matchCnt = 0
-    for(y in space.indices) {
-        for(x in space[y].indices) {
-            if(space[y][x] == puzzle[y][x]) return 0
-
-            if(space[y][x] == 0) matchCnt++
+        visited = MutableList<MutableList<Boolean>>(yLength) {
+            MutableList<Boolean>(xLength) { false }
         }
-    }
+        for(y in 0 until yLength) {
+            for(x in 0 until xLength) {
+                if(visited[y][x] || table[y][x] == 0) continue
 
-    return matchCnt
-}
-
-fun rotate(idx: Int): MutableList<MutableList<Int>> {
-    val xLen = puzzleList[idx][0].size
-    val yLen = puzzleList[idx].size
-    val tmp = MutableList<MutableList<Int>>(xLen) {
-        MutableList<Int>(yLen) { 0 }
-    }
-
-    for(x in 0 until xLen) {
-        for(y in 0 until yLen) {
-            tmp[x][y] = puzzleList[idx][yLen-y-1][x]
+                puzzleList.add(makePuzzlePiece(y, x, visited, table))
+            }
         }
+
+        usedPuzzle = MutableList<Boolean>(puzzleList.size) { false }
+
+        puzzleList.forEach {
+            it.forEach {
+                println(it)
+            }
+            println()
+        }
+
+        backtracking(0, 0)
+        return result
     }
 
-    return tmp
-}
+    fun backtracking(idx: Int, sum: Int) {
+        result = result.coerceAtLeast(sum)
 
+        if(idx == spaceList.size) return
 
-fun makePuzzlePiece(table: Array<IntArray>, visited: MutableList<MutableList<Boolean>>, y: Int, x: Int): MutableList<MutableList<Int>> {
-
-    val yxList = mutableListOf<Pair<Int, Int>>()
-
-    var minY = y
-    var maxY = y
-    var minX = x
-    var maxX = x
-
-    val deque = ArrayDeque<Pair<Int, Int>>()
-    deque.add(Pair(y, x))
-    visited[y][x] = true
-    yxList.add(Pair(y, x))
-
-    while(deque.isNotEmpty()) {
-
-        val first = deque.removeFirst()
-        val elementX = first.second
-        val elementY = first.first
-
-        for(i in 0 until 4) {
-            val newElementX = elementX + direction[i][1]
-            val newElementY = elementY + direction[i][0]
-
-            if(newElementX !in 0 until xLength || newElementY !in 0 until yLength) {
+        for(i in puzzleList.indices) {
+            if(usedPuzzle[i]) {
                 continue
             }
 
-            if(visited[newElementY][newElementX] ||  table[newElementY][newElementX] == 0) continue
+            for(j in 0 until 4) {
+                val matchCnt = canMatchBoard(spaceList[idx], i)
 
+                if(matchCnt == 0) {
+                    puzzleList[i] = rotate(i)
+                } else {
+                    usedPuzzle[i] = true
+                    backtracking(idx + 1, sum+matchCnt)
 
-            deque.add(Pair(newElementY, newElementX))
-            visited[newElementY][newElementX] = true
-            yxList.add(Pair(newElementY, newElementX))
-
-            minY = minY.coerceAtMost(newElementY)
-            maxY = maxY.coerceAtLeast(newElementY)
-            minX = minX.coerceAtMost(newElementX)
-            maxX = maxX.coerceAtLeast(newElementX)
-
+                    break
+                }
+            }
         }
+
+        backtracking(idx+1, sum)
     }
 
-    val piece = MutableList<MutableList<Int>>(maxY-minY+1) {
-        MutableList<Int>(maxX-minX+1) { 0 }
-    }
-
-    for((yy, xx) in yxList) {
-        piece[yy-minY][xx-minX] = 1
-    }
-
-    return piece
-}
-
-fun makeSpacePiece(gameBoard: Array<IntArray>, visited: MutableList<MutableList<Boolean>>, y: Int, x: Int): MutableList<MutableList<Int>> {
-
-
-    var minY = y
-    var maxY = y
-    var minX = x
-    var maxX = x
-
-    val yxList = mutableListOf<Pair<Int, Int>>()
-    val deque = ArrayDeque<Pair<Int, Int>>()
-    visited[y][x] = true
-
-    yxList.add(Pair(y, x))
-    deque.add(Pair(y, x))
-
-    while(deque.isNotEmpty()) {
-        val first = deque.removeFirst()
-
-        val elementX = first.second
-        val elementY = first.first
-
-        for(i in 0 until 4) {
-            val newElementY = elementY + direction[i][0]
-            val newElementX = elementX + direction[i][1]
-
-            if(newElementX !in 0 until xLength || newElementY !in 0 until yLength) continue
-
-            if(visited[newElementY][newElementX]) continue
-
-            if(gameBoard[newElementY][newElementX] == 1) continue
-
-            yxList.add(Pair(newElementY, newElementX))
-            deque.add(Pair(newElementY, newElementX))
-            visited[newElementY][newElementX] = true
-
-            minX = minX.coerceAtMost(newElementX)
-            maxX = maxX.coerceAtLeast(newElementX)
-            minY = minY.coerceAtMost(newElementY)
-            maxY = maxY.coerceAtLeast(newElementY)
+    fun rotate(idx: Int): MutableList<MutableList<Int>> {
+        val yLen = puzzleList[idx].size
+        val xLen = puzzleList[idx][0].size
+        val tmp = MutableList<MutableList<Int>>(xLen) {
+            MutableList<Int>(yLen) { 0 }
         }
+
+        for(x in 0 until xLen) {
+            for(y in 0 until yLen) {
+                tmp[x][y] = puzzleList[idx][yLen-y-1][x]
+            }
+        }
+
+        return tmp
     }
 
-    val piece = MutableList<MutableList<Int>>(maxY-minY+1) {
-        MutableList<Int>(maxX-minX+1) { 1 }
+    fun canMatchBoard(space: MutableList<MutableList<Int>>, idx: Int): Int {
+        val puzzle = puzzleList[idx]
+        if(space.size != puzzle.size || space[0].size != puzzle[0].size) {
+            return 0
+        }
+
+        var matchCnt = 0
+        for(y in space.indices) {
+            for(x in space[y].indices) {
+                if(space[y][x] == puzzle[y][x]) {
+                    return 0
+                }
+
+                if(space[y][x] == 0) matchCnt ++
+            }
+        }
+        return matchCnt
     }
 
-    for((yy, xx) in yxList) {
-        piece[yy-minY][xx-minX] = 0
+    fun makePuzzlePiece(y: Int, x: Int, visited: MutableList<MutableList<Boolean>>, table: Array<IntArray>): MutableList<MutableList<Int>> {
+
+        var minY = y
+        var maxY = y
+        var minX = x
+        var maxX = x
+
+        val yxList = ArrayList<Pair<Int, Int>>()
+        val deque = ArrayDeque<Pair<Int, Int>>()
+        yxList.add(Pair(y, x))
+        deque.add(Pair(y, x))
+        visited[y][x] = true
+
+        while(deque.isNotEmpty()) {
+            val first = deque.removeFirst()
+            val elementY = first.first
+            val elementX = first.second
+
+            for(i in 0 until 4) {
+                val newElementY = elementY + direction[i][0]
+                val newElementX = elementX + direction[i][1]
+
+                if(newElementY !in 0 until yLength || newElementX !in 0 until xLength) continue
+
+                if(visited[newElementY][newElementX]) continue
+
+                if(table[newElementY][newElementX] == 0) continue
+
+                yxList.add(Pair(newElementY, newElementX))
+                deque.add(Pair(newElementY, newElementX))
+                visited[newElementY][newElementX] = true
+                minY = minY.coerceAtMost(newElementY)
+                maxY = maxY.coerceAtLeast(newElementY)
+                minX = minX.coerceAtMost(newElementX)
+                maxX = maxX.coerceAtLeast(newElementX)
+            }
+        }
+
+        val piece = MutableList<MutableList<Int>>(maxY-minY+1) {
+            MutableList<Int>(maxX-minX+1) { 0 }
+        }
+
+        for((yy, xx) in yxList) {
+            piece[yy-minY][xx-minX] = 1
+        }
+
+        return piece
     }
 
-    return piece
+    fun makeSpacePiece(y: Int, x: Int, visited: MutableList<MutableList<Boolean>>, gameBoard: Array<IntArray>): MutableList<MutableList<Int>> {
+
+        var minY = y
+        var maxY = y
+        var minX = x
+        var maxX = x
+
+        val yxList = ArrayList<Pair<Int, Int>>()
+        val deque = ArrayDeque<Pair<Int, Int>>()
+        yxList.add(Pair(y, x))
+        deque.add(Pair(y, x))
+        visited[y][x] = true
+
+        while(deque.isNotEmpty()) {
+
+            val first = deque.removeFirst()
+            val elementY = first.first
+            val elementX = first.second
+
+            for(i in 0 until 4) {
+                val newElementY = elementY + direction[i][0]
+                val newElementX = elementX + direction[i][1]
+
+                if(newElementX !in 0 until yLength || newElementY !in 0 until xLength) {
+                    continue
+                }
+
+                if(visited[newElementY][newElementX]) continue
+
+                if(gameBoard[newElementY][newElementX] == 1) continue
+
+                yxList.add(Pair(newElementY, newElementX))
+                deque.add(Pair(newElementY, newElementX))
+                visited[newElementY][newElementX] = true
+
+                minY = minY.coerceAtMost(newElementY)
+                maxY = maxY.coerceAtLeast(newElementY)
+                minX = minX.coerceAtMost(newElementX)
+                maxX = maxX.coerceAtLeast(newElementX)
+            }
+        }
+
+        val piece = MutableList<MutableList<Int>>(maxY-minY+1) {
+            MutableList<Int>(maxX-minX+1) { 1 }
+        }
+
+        for((yy, xx) in yxList) {
+            piece[yy-minY][xx-minX] = 0
+        }
+
+        return piece
+    }
+
+
+
 }
