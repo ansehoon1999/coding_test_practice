3bacdb2816cbf1d063bf0bb5e87c609e7bdfc1c1
안세훈 <ansehoon1999@gmail.com>
2023-08-09 21:35:02 +0900

    2021 카카오 채용연계형 인턴십: 표 편집

--- 프로그래머스 풀이 모음/2021 카카오 채용연계형 인턴십/3.kt
+++ 프로그래머스 풀이 모음/2021 카카오 채용연계형 인턴십/3.kt
@@ -1,157 +1,58 @@
 import java.util.*
-import kotlin.collections.HashMap
-import kotlin.math.min
 
+class Solution {
 
-fun main(args: Array<String>) {
+    private var curPosition = 0
+    private var backStk = Stack<Int>()
+    private var total = 0
 
-    val n = 8
-    val k = 2
-    val cmd = arrayOf("D 2","C","U 3","C","D 4","C","U 2","Z","Z","U 1","C")
+    fun solution(n: Int, k: Int, cmd: Array<String>): String {
 
-    println(solution(n, k, cmd))
-}
+        val resultArray = Array<String>(n) { "O" }
 
-private lateinit var map: HashMap<Int, Boolean>
-private var curPosition = 0
-private var backStk = Stack<Int>()
+        curPosition = k
+        total = n
 
-fun solution(n: Int, k: Int, cmd: Array<String>): String {
+        cmd.forEach {
+            val state = it.split(" ")
+            val op = state[0]
 
-    map = hashMapOf()
-    for(i in 0 until n) {
-        map[i] = true
-    }
+            when(op) {
+                "U" -> {
+                    curPosition -= state[1].toInt()
+                }
+                "D" -> {
+                    curPosition += state[1].toInt()
+                }
+                "C" -> {
+                    total -= 1
+                    backStk.add(curPosition)
+                    if(curPosition == total) {
+                        curPosition -= 1
+                    }
+                }
+                "Z" -> {
+                    val pop = backStk.pop()
 
-    curPosition = k
+                    total += 1
 
-    cmd.forEach {
-        val state = it.split(" ")
-        val op = state[0]
-
-        println(it)
-
-        when(op) {
-            "U" -> {
-                uOperation(state[1].toInt())
-            }
-            "D" -> {
-                dOperation(state[1].toInt())
-            }
-            "C" -> {
-                cOperation(n)
-            }
-            "Z" -> {
-                zOperation()
+                    if(pop <= curPosition) {
+                        curPosition += 1
+                    }
+                }
             }
         }
-    }
 
+        val sb = StringBuilder()
+        sb.append("O".repeat(total))
 
-    println(map)
+        while(!backStk.isEmpty()) {
+            sb.insert(backStk.pop(), 'X')
 
-
-    val str = StringBuilder()
-
-    for(i in 0 until n) {
-        if(map[i] == true) {
-            str.append("O")
-        } else {
-            str.append("X")
-        }
-    }
-
-    return str.toString()
-}
-
-
-
-fun uOperation(uCount: Int) {
-    var count = uCount
-    var tmpPosition = curPosition
-
-    while(count != 0) {
-        val nextPosition = tmpPosition - 1
-        if(map[nextPosition] == true) {
-            count --
         }
 
-        tmpPosition --
+
+        return sb.toString()
     }
-
-    curPosition = tmpPosition
-
-    println("curPosition: ${curPosition}")
-    println("map: ${map}")
-}
-
-fun dOperation(dCount: Int) {
-    var count = dCount
-    var tmpPosition = curPosition
-
-//    println("count: ${count}")
-//    println("tmpPosition: ${tmpPosition}")
-
-    while(count != 0) {
-        val nexPosition = tmpPosition + 1
-        if(map[nexPosition] == true) {
-            count --
-        }
-
-        tmpPosition ++
-    }
-
-    curPosition = tmpPosition
-
-    println("curPosition: ${curPosition}")
-    println("map: ${map}")
 
 }
-
-fun cOperation(n: Int) {
-    map[curPosition] = false
-    backStk.add(curPosition)
-
-    var tmpPosition = curPosition + 1
-    var finalState = false
-
-    while(true) {
-        if(tmpPosition == n) {
-            finalState = true
-            break
-        }
-
-        if(map[tmpPosition] == true) {
-            curPosition = tmpPosition
-            break
-        } else if(map[tmpPosition] == false) {
-            tmpPosition ++
-        }
-    }
-
-    if(finalState) {
-        var reTmpPosition = curPosition - 1
-
-        while(true) {
-            if(map[reTmpPosition] == true) {
-                curPosition = reTmpPosition
-                break
-            } else if(map[reTmpPosition] == false) {
-                reTmpPosition --
-            }
-        }
-    }
-
-    println("curPosition: ${curPosition}")
-    println("map: ${map}")
-
-}
-
-fun zOperation() {
-    val top = backStk.pop()
-    map[top] = true
-
-    println("curPosition: ${curPosition}")
-    println("map: ${map}")
-}
-
