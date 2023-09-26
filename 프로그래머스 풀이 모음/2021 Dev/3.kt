a1dde9e11f670687a81b0dfc11953e1fbed3658d
안세훈 <ansehoon1999@gmail.com>
2023-09-26 21:20:36 +0900

    2021 Dev-matching: 다단계 칫솔 판매

--- 프로그래머스 풀이 모음/2021 Dev/3.kt
+++ 프로그래머스 풀이 모음/2021 Dev/3.kt
@@ -1,73 +1,64 @@
+class Solution {
 
-fun main(args: Array<String>) {
+    private lateinit var childToParent: HashMap<String, String>
+    private lateinit var enrollResult: HashMap<String, Double>
 
+    fun solution(enroll: Array<String>, referral: Array<String>, seller: Array<String>, amount: IntArray): IntArray {
 
-//    val enroll = arrayOf("john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young")
-//    val referral = arrayOf("-", "-", "mary", "edward", "mary", "mary", "jaimie", "edward")
-//    val seller = arrayOf("young", "john", "tod", "emily", "mary")
-//    val amount = intArrayOf(12, 4, 2, 5, 10)
+        childToParent = hashMapOf()
+        enrollResult = hashMapOf()
 
-    val enroll = arrayOf("john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young")
-    val referral = arrayOf("-", "-", "mary", "edward", "mary", "mary", "jaimie", "edward")
-    val seller = arrayOf("sam", "emily", "jaimie", "edward")
-    val amount = intArrayOf(2, 3, 5, 4)
+        for(i in enroll.indices) {
+            childToParent[enroll[i]] = enroll[i]
+            childToParent[enroll[i]] = referral[i]
+            enrollResult[enroll[i]] = 0.0
 
+        }
 
-    println(solution(enroll, referral, seller, amount).toMutableList())
-}
+        for(i in seller.indices) {
+            var curSeller = seller[i]
+            var curAmount = amount[i] * 100
 
-private lateinit var childToParent: HashMap<String, String>
-private lateinit var enrollResult: HashMap<String, Double>
+            find(curSeller, curAmount)
+            //        while(true) {
+            //
+            //            if(curSeller == "-") {
+            //                break
+            //            }
+            //
+            //            val div = curAmount / 10
+            //            val remain = curAmount - div
+            //
+            //            enrollResult[curSeller] = enrollResult[curSeller]!!.plus(remain)
+            //            curAmount = div
+            //
+            //            curSeller = childToParent[curSeller]!!
+            //
+            //        }
+        }
 
-fun solution(enroll: Array<String>, referral: Array<String>, seller: Array<String>, amount: IntArray): IntArray {
+        val answer = IntArray(enroll.size) { 0 }
+        for(i in enroll.indices) {
+            answer[i] = enrollResult[enroll[i]]!!.toInt()
+        }
 
-    childToParent = hashMapOf()
-    enrollResult = hashMapOf()
-
-    for(i in enroll.indices) {
-        childToParent[enroll[i]] = enroll[i]
-        childToParent[enroll[i]] = referral[i]
-        enrollResult[enroll[i]] = 0.0
-
+        return answer
     }
 
-    for(i in seller.indices) {
-        var curSeller = seller[i]
-        var curAmount = amount[i] * 100
+    fun find(curSeller: String, curAmount: Int) {
+        if(curSeller == "-") return
 
-        find(curSeller, curAmount)
-//        while(true) {
-//
-//            if(curSeller == "-") {
-//                break
-//            }
-//
-//            val div = curAmount / 10
-//            val remain = curAmount - div
-//
-//            enrollResult[curSeller] = enrollResult[curSeller]!!.plus(remain)
-//            curAmount = div
-//
-//            curSeller = childToParent[curSeller]!!
-//
-//        }
+        val div = curAmount / 10
+        val remain = curAmount - div
+
+        enrollResult[curSeller] = enrollResult[curSeller]!!.plus(remain)
+
+        if(div < 1) return
+
+        find(childToParent[curSeller]!!, div)
     }
 
-    val answer = IntArray(enroll.size) { 0 }
-    for(i in enroll.indices) {
-        answer[i] = enrollResult[enroll[i]]!!.toInt()
-    }
 
-    return answer
+
+
 }
-
-fun find(curSeller: String, curAmount: Int) {
-    if(curSeller == "-") return
-
-    val div = curAmount / 10
-    val remain = curAmount - div
-
-    enrollResult[curSeller] = enrollResult[curSeller]!!.plus(remain)
-    find(childToParent[curSeller]!!, div)
-}
-
