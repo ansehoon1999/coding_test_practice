f7cb95cdc4ecc6f45fc9194491c013eaa21d7a9a
안세훈 <ansehoon1999@gmail.com>
2023-11-07 20:38:53 +0900

    level1: 제일 작은 수 제거하기

---
+++ 프로그래머스 풀이 모음/연습문제/level1/제일 작은 수 제거하기.kt
@@ -0,0 +1,22 @@
+class Solution {
+    fun solution(arr: IntArray): IntArray {
+
+        var minIdx = 0
+        var minValue = 100000000
+
+        arr.forEachIndexed { idx, num ->
+            if(minValue > num) {
+                minValue = num
+                minIdx = idx
+            }
+
+        }
+
+        val arrList = arr.toMutableList()
+        arrList.removeAt(minIdx)
+
+        if(arrList.isEmpty()) return intArrayOf(-1)
+        else return arrList.toIntArray()
+
+    }
+}
