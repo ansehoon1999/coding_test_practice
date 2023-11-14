0094f1c4b8900e6d12defda2caa16061f1dab1ea
안세훈 <ansehoon1999@gmail.com>
2023-11-14 20:00:27 +0900

    level1: 숫자 문자열과 영단어

---
+++ 프로그래머스 풀이 모음/연습문제/level1/숫자 문자열과 영단어.kt
@@ -0,0 +1,18 @@
+class Solution {
+
+    fun solution(s: String): Int {
+
+        var s = s.replace("zero", "0")
+        s = s.replace("one", "1")
+        s = s.replace("two", "2")
+        s = s.replace("three", "3")
+        s = s.replace("four", "4")
+        s = s.replace("five", "5")
+        s = s.replace("six", "6")
+        s = s.replace("seven", "7")
+        s = s.replace("eight", "8")
+        s = s.replace("nine", "9")
+
+        return s.toInt()
+    }
+}
