81d7db5245525573889fa61f965986c3846be4c3
ansehoon1999 <ansehoon1999@gmail.com>
2023-11-20 14:51:43 +0900

    level2: 가장 큰 수

--- 프로그래머스 풀이 모음/연습문제/level2/다리를 지나는 트럭.kt
+++ 프로그래머스 풀이 모음/연습문제/level2/다리를 지나는 트럭.kt
@@ -1,36 +1,17 @@
-import java.util.*
-
 class Solution {
-    fun solution(bridge_length: Int, weight: Int, truck_weights: IntArray): Int {
-        var answer = 0
+    fun solution(numbers: IntArray): String {
 
-        val bridgeQueue = LinkedList<Int>()
-        val waitQueue = LinkedList<Int>()
+        val result = numbers.map { it.toString() }.sortedWith(Comparator { o1, o2 ->
+            return@Comparator (o2 + o1).compareTo(o1 + o2)
+        })
 
-        repeat(bridge_length) {
-            bridgeQueue.add(0)
+        if(result[0] == "0") return "0"
+
+        val sb = StringBuilder()
+        result.forEach {
+            sb.append(it)
         }
 
-        truck_weights.forEach {
-            waitQueue.add(it)
-        }
-
-        while(bridgeQueue.isNotEmpty()) {
-
-            answer ++
-            bridgeQueue.poll()
-
-            if(waitQueue.isNotEmpty()) {
-                if(bridgeQueue.sum() <= weight - waitQueue.peek()) {
-                    bridgeQueue.add(waitQueue.poll())
-                } else {
-                    bridgeQueue.add(0)
-                }
-            }
-
-        }
-
-
-        return answer
+        return sb.toString()
     }
 }
