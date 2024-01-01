d5d99aadad77e67bb2188eb697a0c78148a4391d
안세훈 <ansehoon1999@gmail.com>
2024-01-01 09:19:45 +0900

    level2: 요격 시스템

--- 프로그래머스 풀이 모음/연습문제/level2/요격 시스템.kt
+++ 프로그래머스 풀이 모음/연습문제/level2/요격 시스템.kt
@@ -2,20 +2,34 @@ class Solution {
     fun solution(targets: Array<IntArray>): Int {
         var answer: Int = 0
 
+        targets.sortBy { it[0] }
         targets.sortBy { it[1] }
 
-        var cnt = 0
-        var start = 0
-        var end = 0
+        // targets.forEach {
+        //     println(it.toList())
+        // }
 
-        for(idx in 0 until targets.size) {
-            if(targets[idx][0] >= end) {
-                cnt ++
-                end = targets[idx][1]
+        var befStart = 0
+        var befEnd = 0
+
+        targets.forEachIndexed { idx, target ->
+            val curStart = target[0]
+            val curEnd = target[1]
+
+            if(idx == 0) {
+                befStart = curStart
+                befEnd = curEnd
+                answer ++
+            } else {
+                if(befEnd <= curStart) {
+                    befStart = curStart
+                    befEnd = curEnd
+                    answer ++
+                }
             }
-
         }
 
-        return cnt
+
+        return answer
     }
 }
