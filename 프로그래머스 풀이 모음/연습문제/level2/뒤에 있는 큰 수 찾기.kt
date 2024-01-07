ca7314930744cab4efaad55fb7380448ceda8baa
안세훈 <ansehoon1999@gmail.com>
2024-01-07 21:33:49 +0900

    level2: 뒤에 있는 큰 수 찾기

--- 프로그래머스 풀이 모음/연습문제/level2/뒤에 있는 큰 수 찾기.kt
+++ 프로그래머스 풀이 모음/연습문제/level2/뒤에 있는 큰 수 찾기.kt
@@ -1,22 +1,42 @@
 import java.util.*
 
 class Solution {
-    fun solution(numbers: IntArray): IntArray {
-        var answer = Array<Int>(numbers.size) { -1 }
 
-        val stk = Stack<Int>()
+    val stk = Stack<Pair<Int, Int>>()
+
+    fun solution(numbers: IntArray): IntArray {
+        var answer: IntArray = IntArray(numbers.size)
 
         for(idx in 0 until numbers.size) {
 
-            while(stk.isNotEmpty() && numbers[stk.peek()] < numbers[idx]) {
-                answer[stk.pop()] = numbers[idx]
+            while(stk.isNotEmpty()) {
+
+                val last = stk.last()
+                val lastIdx = last.first
+                val lastNum = last.second
+
+                if(numbers[idx] > lastNum) {
+                    answer[lastIdx] = numbers[idx]
+                    stk.pop()
+                } else {
+                    break
+                }
             }
 
-            stk.add(idx)
+            stk.push(Pair(idx, numbers[idx]))
 
+            // println("answer: ${answer.toList()}")
+            // println("stk: ${stk.toList()}")
         }
 
+        while(stk.isNotEmpty()) {
+            val last = stk.pop()
+            val idx = last.first
+            val cost = last.second
 
-        return answer.toIntArray()
+            answer[idx] = -1
+        }
+
+        return answer
     }
 }
