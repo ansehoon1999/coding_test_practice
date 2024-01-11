8356791287461a428514b789c42263f6c759b359
안세훈 <ansehoon1999@gmail.com>
2024-01-11 21:22:48 +0900

    level2: 수식 최대화

--- 프로그래머스 풀이 모음/연습문제/level2/수식 최대화.kt
+++ 프로그래머스 풀이 모음/연습문제/level2/수식 최대화.kt
@@ -1,83 +1,107 @@
+import java.util.*
+
 class Solution {
 
     val hashMap = hashMapOf<Set<String>, Array<Array<String>>>()
 
-    fun solution(expression: String): Long {
-        var answer: Long = Long.MIN_VALUE
+    var answer: Long = Long.MIN_VALUE
 
-        hashMap[setOf("+")] = arrayOf(arrayOf("+"))
+    fun solution(expression: String): Long {
+
+
         hashMap[setOf("-")] = arrayOf(arrayOf("-"))
+        hashMap[setOf("+")] = arrayOf(arrayOf("+"))
         hashMap[setOf("*")] = arrayOf(arrayOf("*"))
 
-        hashMap[setOf("+", "-")] = arrayOf(arrayOf("+", "-"), arrayOf("-", "+"))
-        hashMap[setOf("+", "*")] = arrayOf(arrayOf("+", "*"), arrayOf("*", "+"))
+        hashMap[setOf("-", "+")] = arrayOf(arrayOf("-", "+"), arrayOf("+", "-"))
         hashMap[setOf("-", "*")] = arrayOf(arrayOf("-", "*"), arrayOf("*", "-"))
+        hashMap[setOf("+", "*")] = arrayOf(arrayOf("+", "*"), arrayOf("*", "+"))
 
-        hashMap[setOf("+", "-", "*")] = arrayOf(
-                arrayOf("+", "-", "*"),
-                arrayOf("+", "*", "-"),
+        hashMap[setOf("-", "+", "*")] = arrayOf(
                 arrayOf("-", "+", "*"),
                 arrayOf("-", "*", "+"),
-                arrayOf("*", "+", "-"),
+                arrayOf("+", "-", "*"),
+                arrayOf("+", "*", "-"),
                 arrayOf("*", "-", "+"),
+                arrayOf("*", "+", "-")
         )
 
-        val expressionList = expression.toList()
+        val expressionList = mutableListOf<String>()
+        val operationSet = mutableSetOf<String>()
 
-        val expressionSet = mutableSetOf<String>()
-        val expressionValueList = mutableListOf<String>()
-        var num = ""
 
-        for(idx in 0 until expressionList.size) {
-            if(expressionList[idx] == '+' || expressionList[idx] == '-' || expressionList[idx] == '*') {
-                expressionSet.add(expressionList[idx].toString())
-                expressionValueList.add(num)
-                expressionValueList.add(expressionList[idx].toString())
-
-                num = ""
+        var tmp = ""
+        for(idx in 0 until expression.length) {
+            if(expression[idx] == '-' || expression[idx] == '*' || expression[idx] == '+') {
+                expressionList.add(tmp)
+                expressionList.add(expression[idx].toString())
+                operationSet.add(expression[idx].toString())
+                tmp = ""
             } else {
-                num += expressionList[idx].toString()
+                tmp = tmp + expression[idx].toString()
             }
         }
+        expressionList.add(tmp)
 
-        expressionValueList.add(num)
+//         println(expressionList)
+//         println(operationSet)
 
-        val curOperationList = hashMap[expressionSet]!!
+//         for((key, value) in hashMap) {
+//             println("key: ${key}, value: ${value.toList().map { it.toList() }}")
+//         }
 
-        curOperationList.forEach { subList ->
+        val caseList = hashMap[operationSet.toSet()]!!
+        // println(caseList.toList().map { it.toList()})
 
-            var initList = expressionValueList
-
-            subList.forEach { op ->
-
-                val tmpList = mutableListOf<String>(initList[0])
-
-                for(idx in 1 until initList.size step(2)) {
-                    if(op.toString() == initList[idx].toString()) {
-                        val lastIdx = tmpList.size - 1
-                        val lastNum = tmpList.last()
-
-                        if(op == "+") {
-                            tmpList[lastIdx] = (lastNum.toLong() + initList[idx+1].toLong()).toString()
-                        } else if(op == "-") {
-                            tmpList[lastIdx] = (lastNum.toLong() - initList[idx+1].toLong()).toString()
-                        } else if(op == "*") {
-                            tmpList[lastIdx] = (lastNum.toLong() * initList[idx+1].toLong()).toString()
-                        }
-                    } else {
-                        tmpList.add(initList[idx])
-                        tmpList.add(initList[idx+1])
-                    }
-                }
-
-                initList = tmpList
-            }
-
-            answer = maxOf(answer, Math.abs(initList[0].toLong()))
-
+        caseList.forEach { case ->
+            calculate(case, expressionList, 0)
         }
-
 
         return answer
+    }
+
+    fun calculate(case: Array<String>, expressionList: MutableList<String>, idx: Int) {
+
+        if(idx == case.size) {
+            answer = maxOf(answer, Math.abs(expressionList[0].toLong()))
+            // println(expressionList)
+            return
+        }
+
+        val curOperation = case[idx].toString()
+        val stk = Stack<String>()
+
+        // println("case: ${case.toList()}")
+        // println("expression: ${expressionList}")
+        // println("curOperation: ${curOperation}")
+        // println("idx: ${idx}")
+        // println()
+
+        for(idx in 0 until expressionList.size) {
+
+            if(stk.isNotEmpty()) {
+
+                val last = stk.pop() // -
+                if(last == curOperation) {
+                    val lastNum = stk.pop().toLong()
+                    if(last == "*") {
+                        stk.add((lastNum * expressionList[idx].toLong()).toString())
+                    } else if(curOperation == "-") {
+                        stk.add((lastNum - expressionList[idx].toLong()).toString())
+                    } else if(curOperation == "+") {
+                        stk.add((lastNum + expressionList[idx].toLong()).toString())
+                    }
+                } else {
+                    stk.add(last)
+                    stk.add(expressionList[idx])
+                }
+
+            } else {
+                stk.add(expressionList[idx])
+            }
+        }
+
+        calculate(case, stk.toMutableList(), idx + 1)
+
     }
 }
