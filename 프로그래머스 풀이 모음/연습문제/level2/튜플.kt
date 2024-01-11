fb3343678a4e201c6834297db95e67723361cce4
안세훈 <ansehoon1999@gmail.com>
2024-01-11 23:20:22 +0900

    level2: 튜플

--- 프로그래머스 풀이 모음/연습문제/level2/튜플.kt
+++ 프로그래머스 풀이 모음/연습문제/level2/튜플.kt
@@ -2,25 +2,41 @@ class Solution {
     fun solution(s: String): IntArray {
         var answer = intArrayOf()
 
-        val sList = s.substring(1, s.length-1).split("},{")
+        val newStr = s.substring(1, s.length-1).split("},{").toList()
+        // println(newStr)
+        val tmpList = mutableListOf<String>()
 
-        val newList = mutableListOf<List<Int>>()
-
-        sList.forEach { str ->
-            val newStr = str.replace("{", "").replace("}", "")
-            newList.add(newStr.split(",").map { it.toInt() })
+        if(newStr.size == 1) {
+            tmpList.add(newStr[0].substring(1, newStr[0].length-1))
+        } else {
+            for(idx in 0 until newStr.size) {
+                if(idx == 0) {
+                    tmpList.add(newStr[idx].substring(1, newStr[idx].length))
+                } else if (idx == newStr.size - 1) {
+                    tmpList.add(newStr[idx].substring(0, newStr[idx].length - 1))
+                } else {
+                    tmpList.add(newStr[idx])
+                }
+            }
+        }
+        val resultList = mutableListOf<List<String>>()
+        tmpList.forEach {
+            resultList.add(it.split(",").toList())
         }
 
-        newList.sortBy { it.size }
+        resultList.sortBy { it.size }
+        // println(resultList)
+        // println(resultList[0])
 
-        val result = mutableSetOf<Int>()
-
-        newList.forEach { subList ->
-            subList.forEach {
-                result.add(it)
+        answer += resultList[0][0].toInt()
+        for(idx in 1 until resultList.size) {
+            if(idx == 0) {
+                answer += resultList[idx][0].toInt()
+            } else {
+                answer += (resultList[idx] - resultList[idx-1])[0].toInt()
             }
         }
 
-        return result.toIntArray()
+        return answer
     }
 }
