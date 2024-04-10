3d53271efab1f7b0dca3bc2c0d2f75718dfc36c1
안세훈 <ansehoon1999@gmail.com>
2024-04-10 11:09:33 +0900

    level2: n*2 배열 자르기

--- 프로그래머스 풀이 모음/연습문제/level2/n*2 배열 자르기.kt
+++ 프로그래머스 풀이 모음/연습문제/level2/n*2 배열 자르기.kt
@@ -1,18 +1,17 @@
 class Solution {
     fun solution(n: Int, left: Long, right: Long): IntArray {
         var answer = mutableListOf<Int>()
-
-        for(i in (left) .. (right)) {
-
-            val row = (i / n) + 1
-            val col = (i % n) + 1
-
-            val maxNum = maxOf(row, col)
-
-            answer.add(maxNum.toInt())
-
+        
+        for(idx in left .. right) {
+            
+            val col = idx / n
+            val row = idx % n
+                        
+            val maxVal = maxOf(col, row).toInt()
+            answer.add(maxVal + 1)
         }
-
+        
+        
         return answer.toIntArray()
     }
 }
