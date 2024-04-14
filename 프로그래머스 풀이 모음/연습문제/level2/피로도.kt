012be3cef886583f7f1fadc24a346054b937ec19
안세뿡 <ansehun@anseppung-ui-MacBookPro.local>
2024-04-14 14:51:01 +0900

    level2: 피로도

--- 프로그래머스 풀이 모음/연습문제/level2/피로도.kt
+++ 프로그래머스 풀이 모음/연습문제/level2/피로도.kt
@@ -1,39 +1,39 @@
 class Solution {
-
+    
     var answer: Int = Integer.MIN_VALUE
-
+    
     fun solution(k: Int, dungeons: Array<IntArray>): Int {
-
+        
         val visited = MutableList<Boolean>(dungeons.size) { false }
-
-        dfs(k, dungeons, visited, 0)
-
+        
+        dfs(k, dungeons, 0, visited)
+        
         return answer
     }
-
-    fun dfs(k: Int, dungeons: Array<IntArray>, visited: MutableList<Boolean>, cnt: Int) {
-
-        if(k < 0) {
-
-            return
-        }
-
+    
+    fun dfs(k: Int, dungeons: Array<IntArray>, cnt: Int, visited: MutableList<Boolean>) {
+        
+        if(k < 0) return
+        
         for(idx in 0 until dungeons.size) {
-
+            
             val minTired = dungeons[idx][0]
             val consumeTired = dungeons[idx][1]
-
+            
             if(k < minTired) {
                 answer = maxOf(answer, cnt)
                 continue
             }
-
+            
             if(!visited[idx]) {
                 visited[idx] = true
-                dfs(k-consumeTired, dungeons, visited, cnt+1)
+                dfs(k - consumeTired, dungeons, cnt + 1, visited)
                 visited[idx] = false
             }
-
+            
+            
         }
+    
+        
     }
 }
