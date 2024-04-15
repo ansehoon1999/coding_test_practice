0e235925742226ce48211e1b39e013ecbc8c9099
안세훈 <ansehoon1999@gmail.com>
2024-04-15 20:33:17 +0900

    level2: 튜플

--- 프로그래머스 풀이 모음/연습문제/level2/튜플.kt
+++ 프로그래머스 풀이 모음/연습문제/level2/튜플.kt
@@ -1,42 +1,19 @@
 class Solution {
     fun solution(s: String): IntArray {
-        var answer = intArrayOf()
-
-        val newStr = s.substring(1, s.length-1).split("},{").toList()
-        // println(newStr)
-        val tmpList = mutableListOf<String>()
-
-        if(newStr.size == 1) {
-            tmpList.add(newStr[0].substring(1, newStr[0].length-1))
-        } else {
-            for(idx in 0 until newStr.size) {
-                if(idx == 0) {
-                    tmpList.add(newStr[idx].substring(1, newStr[idx].length))
-                } else if (idx == newStr.size - 1) {
-                    tmpList.add(newStr[idx].substring(0, newStr[idx].length - 1))
-                } else {
-                    tmpList.add(newStr[idx])
-                }
-            }
+        val answer = mutableListOf<Int>()
+        
+        val sList = s.substring(2, s.length - 2).split("},{")
+        
+        val comparator: Comparator<String> = compareBy { it.length }
+        val newList = sList.sortedWith(comparator)
+        val newList2 = newList.map { it.split(",").map { it.toInt()} }
+                        
+        answer.add(newList2[0][0])
+        for(idx in 1 until newList2.size) {
+            answer.add((newList2[idx] - newList2[idx-1])[0])
         }
-        val resultList = mutableListOf<List<String>>()
-        tmpList.forEach {
-            resultList.add(it.split(",").toList())
-        }
-
-        resultList.sortBy { it.size }
-        // println(resultList)
-        // println(resultList[0])
-
-        answer += resultList[0][0].toInt()
-        for(idx in 1 until resultList.size) {
-            if(idx == 0) {
-                answer += resultList[idx][0].toInt()
-            } else {
-                answer += (resultList[idx] - resultList[idx-1])[0].toInt()
-            }
-        }
-
-        return answer
+        
+            
+        return answer.toIntArray()
     }
 }
