87b892897621ac8db25f527fec7926ea35d29581
ansehun <ansehoon1999@gmail.com>
2024-05-07 20:39:13 +0900

    level2: 숫자 변환하기

--- 프로그래머스 풀이 모음/연습문제/level2/숫자 변환하기.kt
+++ 프로그래머스 풀이 모음/연습문제/level2/숫자 변환하기.kt
@@ -1,42 +1,59 @@
 import java.util.*
 
 class Solution {
-
-    lateinit var visited: BooleanArray
-
     fun solution(x: Int, y: Int, n: Int): Int {
-        var answer: Int = 0
-
-        visited = BooleanArray(1000001) { false }
-
+        var answer: Int = Integer.MAX_VALUE
+        
+        val visited = MutableList<Boolean>(y+1) { false }
+        
         val deque = ArrayDeque<Pair<Int, Int>>()
         deque.add(Pair(x, 0))
         visited[x] = true
-
+        
         while(deque.isNotEmpty()) {
-
-            val cur = deque.poll()
-
-            if(cur.first == y) return cur.second
-
-            if((cur.first + n) <= y && !visited[cur.first + n]) {
-                deque.add(Pair(cur.first + n, cur.second + 1))
-                visited[cur.first + n] = true
+            // println(deque)
+            val (curNum, cnt) = deque.removeFirst()
+            
+            if(curNum > y) continue
+            
+            if(curNum == y) {
+                answer = minOf(answer, cnt)
+                continue
             }
-
-            if((cur.first * 2) <= y && !visited[cur.first * 2]) {
-                deque.add(Pair(cur.first * 2, cur.second + 1))
-                visited[cur.first * 2] = true
+            
+            val num1 = calculatePlusN(curNum, n)
+            if(num1 <= y && !visited[num1]) {
+                deque.add(Pair(num1, cnt + 1))
+                visited[num1] = true
             }
-
-            if((cur.first * 3) <= y && !visited[cur.first * 3]) {
-                deque.add(Pair(cur.first * 3, cur.second + 1))
-                visited[cur.first * 3] = true
+            
+            val num2 = calculateMultiple2(curNum)
+            if(num2 <= y && !visited[num2]) {
+                deque.add(Pair(num2, cnt + 1))
+                visited[num2] = true
+            }
+            
+            val num3 = calculateMultiple3(curNum)
+            if(num3 <= y && !visited[num3]) {
+                deque.add(Pair(num3, cnt + 1))
+                visited[num3] = true
             }
         }
-
-
-
-        return -1
+        
+        if(answer == Integer.MAX_VALUE) return -1
+        else return answer
+    }
+    
+    
+    fun calculatePlusN(x: Int, n: Int): Int {
+        return x + n
+    }
+    
+    fun calculateMultiple2(x: Int): Int {
+        return 2 * x
+    }
+    
+    fun calculateMultiple3(x: Int): Int {
+        return 3 * x
     }
 }
