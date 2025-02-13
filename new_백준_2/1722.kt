a1e86f1f14bbb374e62bc2abdb23ca591fbc4be3
ansehun <ansehoon1999@gmail.com>
2025-02-13 20:45:24 +0900

    1722

--- new_백준_2/1722.kt
+++ new_백준_2/1722.kt
@@ -2,86 +2,85 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.util.StringTokenizer
 
+
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val writer = System.out.bufferedWriter()
 
     val n = br.readLine().toInt()
 
-    val arr = mutableListOf<Int>()
-    for (i in 1..n) arr.add(i)
+    val list = MutableList<Int>(n) { it + 1 }
 
-    val st = StringTokenizer(br.readLine())
-    val k = st.nextToken().toInt()
+    val str = StringTokenizer(br.readLine())
+    val op = str.nextToken().toInt()
 
-    when (k) {
-        1 -> {
-            val target = st.nextToken().toLong()
+    if (op == 1) {
+        val target = str.nextToken().toLong()
+        val answer = MutableList<Int>(n) { 0 }
 
-            val answer = IntArray(n)
-
-            solve1(arr, answer, n.toLong(), target, 0)
-
-            answer.forEach {
-                print("${it} ")
-            }
-
+        solve1(list, target, answer)
+        answer.forEach {
+            print("${it} ")
+        }
+    } else if (op == 2) {
+        val target = mutableListOf<Long>()
+        repeat(n) {
+            target.add(str.nextToken().toLong())
         }
 
-        2 -> {
-
-            val target = mutableListOf<Int>()
-            repeat(n) {
-                val num = st.nextToken().toInt()
-                target.add(num)
-            }
-
-            println(solve2(arr, target, 0, 1))
-        }
-
-        else -> {}
+        println(solve2(list, target, 1L))
     }
-
-
-    writer.flush()
-    writer.close()
 }
 
-fun solve1(arr: MutableList<Int>, answer: IntArray, n: Long, target: Long, idx: Int) {
-//    println("arr: ${arr}, answer: ${answer.toList()}, n: ${n}, target: ${target}, idx: ${idx}")
 
-    if(arr.isEmpty()) {
+fun solve1(list: MutableList<Int>, target: Long, answer: MutableList<Int>) {
+//    println("list: ${list}, target: ${target}, answer: ${answer}")
+
+    if(list.isEmpty()) {
         return
     }
 
-    val fac = factorial(arr.size.toLong()-1)
-    val k = (target-1) / fac
-    val x = arr.removeAt(k.toInt())
-    answer[idx] = x
+    val fac = factorial(list.size.toLong() - 1)
+    val idx = (target - 1) / fac
+    val x = list.removeAt(idx.toInt())
+    answer[answer.size - list.size - 1] = x
 
-    solve1(arr, answer, n, target - (k * fac), idx + 1)
+    solve1(list, target - fac * idx, answer)
 }
 
-fun solve2(arr: MutableList<Int>, target: MutableList<Int>, depth: Int, answer: Long): Long {
+fun solve2(list: MutableList<Int>, target: MutableList<Long>, answer: Long): Long {
+//    println("list: ${list}, target: ${target}, answer: ${answer}")
 
-    if(arr.isEmpty()) {
+    if(list.isEmpty()) {
         return answer
     }
 
-    val fac = factorial(arr.size.toLong() - 1)
-    val a = arr.indexOf(target[depth])
-    arr.remove(target[depth])
+    val fac = factorial(list.size.toLong() - 1)
+    val x = target[target.size - list.size].toInt()
+    val idx = list.indexOf(x)
+    list.remove(x)
 
-    return solve2(arr, target, depth+1, answer + (a * fac))
+    return solve2(list, target, answer + (idx * fac))
 }
-
 
 
 fun factorial(n: Long): Long {
-
-    var ans = 1L
-    for (i in 1..n) {
-        ans *= i
+    if(n == 0L) {
+        return 1
     }
-    return ans
+
+    return n * factorial(n-1)
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
